5317c0988fdea3d90b6c976cc3a1f85c4b641327
Aashish <masai@Masais-MacBook-Pro.local>
2024-07-06 12:17:27 +0530

    basic setup for backend

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+import { connect } from "mongoose"
+
+
+const ConnectToDb=async(url)=>{
+
+      await connect(url)
+}
+
+
+export default ConnectToDb;
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { config } from "dotenv";
+import ConnectToDb from "./config/db.js";
+config();
+const app = express();
+const port = process.env.PORT;
+const db_uri = process.env.DB_URI;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("this is a home route");
+});
+
+app.listen(port, async () => {
+  try {
+    await ConnectToDb(db_uri);
+    console.log("connected to database successfully");
+    console.log(`server is running at port ${port}`);
+  } catch (err) {
+    console.log(err);
+  }
+});

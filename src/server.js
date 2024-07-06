1c1bae1875ea6c17af96c9b7f8399c254b8ed4e5
Aashish <masai@Masais-MacBook-Pro.local>
2024-07-06 13:36:25 +0530

    added the cors middleware

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ import ConnectToDb from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
 import taskRouter from "./routes/taskRoute.js";
 import auth from "./middlewares/auth.js";
+import cors from "cors";
 config();
 const app = express();
 const port = process.env.PORT;
 const db_uri = process.env.DB_URI;
 
+app.use(cors({ origin: process.env.FRONTEND_URL, Credentials: true }));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("this is a home route");
